37c015809f731dd941722b1827e9100be7331916
Dima Hubenia <dima96gubenya@gmail.com>
2023-12-28 16:56:28 +0200

    progressView

---
+++ ios-mod-gacha-ref-0-1/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  ios-mod-gacha-ref-0-1
+//
+//  Created by Дима Губеня on 28.12.2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
