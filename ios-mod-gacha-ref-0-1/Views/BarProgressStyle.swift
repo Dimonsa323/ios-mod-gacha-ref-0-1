d45193c667f4098cb66937d7b9ff10fa1bb27445
Dima Hubenia <dima96gubenya@gmail.com>
2023-12-29 17:20:19 +0200

    loading animation

---
+++ ios-mod-gacha-ref-0-1/Views/BarProgressStyle.swift
@@ -0,0 +1,18 @@
+//
+//  BarProgressStyle.swift
+//  ios-mod-gacha-ref-0-1
+//
+//  Created by Дима Губеня on 28.12.2023.
+//
+
+import SwiftUI
+
+struct BarProgressStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarProgressStyle()
+}
