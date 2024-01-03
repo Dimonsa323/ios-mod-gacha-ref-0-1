d380ff12a6072c8ffe2971435d9c313ce2122f17
Dima Hubenia <dima96gubenya@gmail.com>
2024-01-03 17:03:19 +0200

    tabBar

---
+++ ios-mod-gacha-ref-0-1/Views/TabBarScreens/GamesView.swift
@@ -0,0 +1,18 @@
+//
+//  GamesView.swift
+//  ios-mod-gacha-ref-0-1
+//
+//  Created by Дима Губеня on 03.01.2024.
+//
+
+import SwiftUI
+
+struct GamesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GamesView()
+}
---
+++ ios-mod-gacha-ref-0-1/Views/TabBarScreens/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  ios-mod-gacha-ref-0-1
+//
+//  Created by Дима Губеня on 03.01.2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
