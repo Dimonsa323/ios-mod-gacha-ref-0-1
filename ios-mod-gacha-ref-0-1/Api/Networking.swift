051190a47b7c9c6edbf70c45433b01aa9100e213
Dima Hubenia <dima96gubenya@gmail.com>
2024-01-23 16:49:37 +0200

    networking/utilities

---
+++ ios-mod-gacha-ref-0-1/Api/Networking.swift
@@ -0,0 +1,8 @@
+//
+//  Networking.swift
+//  ios-mod-gacha-ref-0-1
+//
+//  Created by Дима Губеня on 23.01.2024.
+//
+
+import Foundation
---
+++ ios-mod-gacha-ref-0-1/Api/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  ios-mod-gacha-ref-0-1
+//
+//  Created by Дима Губеня on 23.01.2024.
+//
+
+import Foundation
